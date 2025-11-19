b685600dde3cc2d9d78b2fcc026fc03cf2793ec3
Rapha835 <71450035+Rapha835@users.noreply.github.com>
2025-11-19 14:10:03 +0100

    Rename CGVSHort import to CGVShort

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import CGVSHort from "./pages/cgv";
+import CGVShort from "./pages/cgv";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
